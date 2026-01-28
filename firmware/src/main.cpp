a10b9da9eb732724bef9634871dfdba67883d38b
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2026-01-27 16:35:34 -0800

    Simple limit switch implemenation

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,53 +1,29 @@
 #include <main.h>
 
+#define LIMIT_SWITCH_PIN 5
+
+int carriage_dir = 1;
+
 void setup() {
+	pinMode(2, OUTPUT); // LED pin
+  pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
+
 	initSteppers();
 
-	pinMode(2, OUTPUT); // LED pin
+  carriageStepper.setSpeed(CARRIAGE_MOTOR_PARAMS.microStepsPerRev * 0.5 * carriage_dir);
+  mandrelStepper.setSpeed(MANDREL_MOTOR_PARAMS.microStepsPerRev * 0.5 * carriage_dir);
 }
 
 void loop() {
-  mandrelStepper.runSpeed();
   carriageStepper.runSpeed();
+  mandrelStepper.runSpeed();
+
+  if (digitalRead(LIMIT_SWITCH_PIN) == HIGH) {
+    carriage_dir = -carriage_dir;
+    carriageStepper.setSpeed(CARRIAGE_MOTOR_PARAMS.microStepsPerRev * 0.5 * carriage_dir);
+    mandrelStepper.setSpeed(MANDREL_MOTOR_PARAMS.microStepsPerRev * 0.5 * carriage_dir);
+
+    // sleep 0.5s
+    delay(500);
+  }
 }
-
-// ---------------
-// ---------------
-
-// Below is simpler code that is confirmed to work
-
-// ---------------
-// ---------------
-
-// #include <AccelStepper.h>
-
-// // Define pin numbers
-// const int stepPin = 25;
-// const int directionPin = 26;
-// const int enablePin = 27; // Optional enable pin
-
-// // Define the stepper interface type (DRIVER for step/dir pins) and the pins
-// AccelStepper stepper(AccelStepper::DRIVER, stepPin, directionPin);
-
-// void setup() {
-//   Serial.begin(9600);
-//   // Set the enable pin and invert its logic (if the driver needs a LOW signal to be enabled)
-  
-//   stepper.setPinsInverted(false, false, true); // Invert enable pin logic
-//   stepper.setEnablePin(enablePin);
-//   stepper.enableOutputs(); // Enable the motor driver outputs
-
-//   // set pin 2 to output and turn it on with arduino gpio
-//   pinMode(2, OUTPUT); // LED pin
-//   digitalWrite(2, HIGH); // Turn on LED
-
-//   // Set the maximum speed in steps per second
-//   stepper.setMaxSpeed(1000); 
-//   // Set the target speed in steps per second
-//   stepper.setSpeed(200); 
-// }
-
-// void loop() {
-//   // Must be called repetitively to make the motor move
-//   stepper.runSpeed();
-// }
